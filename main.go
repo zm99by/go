698f3741db3071b750b4f6391b70267944890bf9
Uladzimir Sakhonchyk <33266845+zm99by@users.noreply.github.com>
2024-02-25 12:51:42 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	arg := os.Args[1]
+
+	if len(os.Args) < 1 {
+		fmt.Println("please added filename")
+		os.Exit(1)
+	}
+
+	dat, err := os.ReadFile(arg)
+	check(err)
+	fmt.Print(string(dat))
+
+}
